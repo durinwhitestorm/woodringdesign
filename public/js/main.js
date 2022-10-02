2c1785dad12bb8fdd6cca8376473be8ead15e8d8
[Chris Becker} <oneringtobeckerthem@gmail.com>
2022-10-02 09:10:04 -0500

    setting up main.js interaction

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,64 @@
-const materialItem = document.querySelectorAll('.materialListItem')
-const ringSize = document.querySelector(".ringSize")
-const ringWidth = document.querySelector(".ringWidth")
-const ringTitle = document.querySelector(".ringTitle")
-const layerSelect = document.querySelector("#layerSelection")
+let ringSize 
+let ringWidth 
+let ringTitle 
+let layerSelect 
+
+const materialItem = document.querySelectorAll('.materialImage')
 const removeLayer = document.querySelector('.removeLayer')
 
+Array.from(materialItem).forEach((material)=>{
+    material.addEventListener('click', setLayerImage)
+})
+document.querySelector(".ringSize").addEventListener('change', setRingSize)
+document.querySelector(".ringWidth").addEventListener('change', setRingWidth)
+document.querySelector(".ringTitle").addEventListener('change', setRingTitle)
+document.querySelector("#layerSelection").addEventListener('change', setZLayer)
 
+// document.querySelector('.removeLayer').addEventListener('click', setLayerImage)
 
+function setZLayer(){
+
+}
+
+function setLayerImage(){
+    const materialImage = this.src
+    document.querySelector('.layerCore').style.backgroundImage = `url(${materialImage})`
+}
+
+//async function markComplete(){
+    //     const todoId = this.parentNode.dataset.id
+    //     try{
+    //         const response = await fetch('todos/markComplete', {
+    //             method: 'put',
+    //             headers: {'Content-type': 'application/json'},
+    //             body: JSON.stringify({
+    //                 'todoIdFromJSFile': todoId
+    //             })
+    //         })
+    //         const data = await response.json()
+    //         console.log(data)
+    //         location.reload()
+    //     }catch(err){
+    //         console.log(err)
+    //     }
+    // }
+
+function changeMaterialList(){
+
+}
+
+function setRingSize(size){
+
+}
+
+function setRingWidth(width){
+
+}
+
+function setRingTitle(name){
+    //May not need if .onChange will work
+    //May be able to tie in with Save button
+}
 
 
 
